5e75b82c6b1cb3085be4bf6ea8c617e3c461048e
Joel Torres <thereaderboy.7@outlook.com>
2022-05-31 21:24:07 -0500

    Set App Launcher Icon, Add Splash Screen, Add Base Welcome Screen.
    Signed-off-by: Joel Torres <thereaderboy.7@outlook.com>

--- app/src/main/java/com/mincal/app/MainActivity.java
+++ app/src/main/java/com/mincal/app/MainActivity.java
@@ -2,7 +2,9 @@ package com.mincal.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Redirect to Home Activity
+
+        Thread background = new Thread() {
+            public void run() {
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5*1000);
+
+                    // After 5 seconds redirect to another intent
+
+                    Intent i = new Intent(getBaseContext(), WelcomeActivity.class);
+                    startActivity(i);
+
+                    //Remove activity
+
+                    finish();
+                } catch (Exception e) {
+                    Log.e(e.toString(), e.getMessage());
+                }
+            }
+        };
+        // start thread
+        background.start();
     }
 }
