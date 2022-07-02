4a1c9b3c6f17516627b94da2ad333cdd701c20fd
Joel Torres <thereaderboy.7@outlook.com>
2022-07-02 18:17:32 -0500

    Add feedback to color selection on SetColor fragment.
    Signed-off-by: Joel Torres <thereaderboy.7@outlook.com>

--- app/src/main/java/com/mincal/app/SetColor.java
+++ app/src/main/java/com/mincal/app/SetColor.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,6 +28,12 @@ public class SetColor extends Fragment {
     View greenColor;
     View blueColor;
     View yellowColor;
+
+    TextView selectColor;
+
+    // Variables
+
+    private static String selectedColor = "";
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -69,15 +77,103 @@ public class SetColor extends Fragment {
         blueColor = getView().findViewById(R.id.blue_color);
         yellowColor = getView().findViewById(R.id.yellow_color);
 
+        selectColor = getView().findViewById(R.id.color_select);
+
         // Listeners
 
         arrowBackField.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                SetField setFieldFragment = new SetField();
+                SetColor setColorFragment = new SetColor();
+                fragmentTransaction.remove(setColorFragment);
+                fragmentTransaction.replace(R.id.get_started_container, setFieldFragment).commit();
             }
         });
 
+        orangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColor = "orange";
+                updateColors();
+            }
+        });
+
+        redColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColor = "red";
+                updateColors();
+            }
+        });
+
+        purpleColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColor = "purple";
+                updateColors();
+            }
+        });
+
+        greenColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColor = "green";
+                updateColors();
+            }
+        });
+
+        blueColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColor = "blue";
+                updateColors();
+            }
+        });
+
+        yellowColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedColor = "yellow";
+                updateColors();
+            }
+        });
+
+    }
+
+    public void refreshRoles() {
+        orangeColor.setBackgroundResource(R.drawable.orange_orb);
+        redColor.setBackgroundResource(R.drawable.red_orb);
+        purpleColor.setBackgroundResource(R.drawable.purple_orb);
+        greenColor.setBackgroundResource(R.drawable.green_orb);
+        blueColor.setBackgroundResource(R.drawable.blue_orb);
+        yellowColor.setBackgroundResource(R.drawable.yellow_orb);
+    }
+
+    public void updateColors() {
+
+        refreshRoles(); // Deselect all roles.
+
+        if (!selectedColor.isEmpty()) {
+            selectColor.setBackgroundResource(R.drawable.rounded_button);
+            selectColor.setPadding(0, 0, 10, 10);
+        }
+
+        if (selectedColor == "orange") {
+            orangeColor.setBackgroundResource(R.drawable.orange_orb_selected);
+        } else if (selectedColor == "red") {
+            redColor.setBackgroundResource(R.drawable.red_orb_selected);
+        } else if (selectedColor == "purple") {
+            purpleColor.setBackgroundResource(R.drawable.purple_orb_selected);
+        } else if (selectedColor == "green") {
+            greenColor.setBackgroundResource(R.drawable.green_orb_selected);
+        } else if (selectedColor == "blue") {
+            blueColor.setBackgroundResource(R.drawable.blue_orb_selected);
+        } else if (selectedColor == "yellow") {
+            yellowColor.setBackgroundResource(R.drawable.yellow_orb_selected);
+        }
     }
 
     @Override
