b2a1339d981c93358029b8966344b47577e81d60
Joel Torres <thereaderboy.7@outlook.com>
2022-07-06 16:20:11 -0500

    Add first iteration of the pad for the calculator as XML.

--- app/src/main/java/com/mincal/app/Calculator.java
+++ app/src/main/java/com/mincal/app/Calculator.java
@@ -1,6 +1,8 @@
 package com.mincal.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,20 @@ public class Calculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        // Fill containers with the users' calculator.
+
+        startCalculator();
+    }
+
+    private void startCalculator() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        // fragmentTransaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out);
+        CalculatorPad calculatorPad = new CalculatorPad();
+        CalculatorScreen calculatorScreen = new CalculatorScreen();
+        fragmentTransaction.replace(R.id.calculator_screen_container, calculatorScreen);
+        fragmentTransaction.replace(R.id.calculator_pad_container, calculatorPad);
+        fragmentTransaction.commit();
     }
 }
