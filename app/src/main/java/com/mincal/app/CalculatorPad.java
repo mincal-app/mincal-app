7c80b04f15f6a8aae5484593f0dee48557c72b25
Joel Torres <thereaderboy.7@outlook.com>
2022-07-07 10:30:39 -0500

    Change the color of the buttons and special tools on CalculatorPad based on preferred color.

--- app/src/main/java/com/mincal/app/CalculatorPad.java
+++ app/src/main/java/com/mincal/app/CalculatorPad.java
@@ -10,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -19,6 +20,8 @@ import java.util.ArrayList;
 public class CalculatorPad extends Fragment {
 
     // Views
+
+    // Number Pad
 
     TextView one;
     TextView two;
@@ -30,6 +33,25 @@ public class CalculatorPad extends Fragment {
     TextView eight;
     TextView nine;
     TextView zero;
+    TextView power;
+
+    // Rounded Buttons
+
+    TextView parentheses;
+    TextView divide;
+    TextView multiply;
+    TextView subtract;
+    TextView add;
+    TextView factorial;
+    TextView euler;
+    TextView root;
+
+    // Special Buttons
+
+    ImageView back;
+    TextView clean;
+    TextView powertools;
+    TextView result;
 
     // TinyDB
 
@@ -81,10 +103,30 @@ public class CalculatorPad extends Fragment {
         eight = getView().findViewById(R.id.pad_numbers_eight);
         nine = getView().findViewById(R.id.pad_numbers_nine);
         zero = getView().findViewById(R.id.pad_numbers_zero);
+        power = getView().findViewById(R.id.pad_numbers_power);
+
+        // Rounded Buttons
+
+        parentheses = getView().findViewById(R.id.pad_top_parentheses);
+        divide = getView().findViewById(R.id.pad_top_divide);
+        multiply = getView().findViewById(R.id.pad_top_multiply);
+        subtract = getView().findViewById(R.id.pad_right_minus);
+        add = getView().findViewById(R.id.pad_right_plus);
+        factorial = getView().findViewById(R.id.pad_bottom_factorial);
+        euler = getView().findViewById(R.id.pad_bottom_euler);
+        root = getView().findViewById(R.id.pad_bottom_root);
+
+        // Special Buttons
+
+        back = getView().findViewById(R.id.pad_numbers_back);
+        clean = getView().findViewById(R.id.pad_top_clean);
+        powertools = getView().findViewById(R.id.pad_right_more_tools);
+        result = getView().findViewById(R.id.pad_right_equal);
 
         // Elements as Array
 
-        TextView[] numberPad = {one, two, three, four, five, six, seven, eight, nine, zero};
+        TextView[] numberPad = {one, two, three, four, five, six, seven, eight, nine, zero, power};
+        TextView[] roundedButtonsPad = {parentheses, divide, multiply, subtract, add, factorial, euler, root, result};
 
         // Instance of SharedPreferences to store the user's name.
 
@@ -109,6 +151,56 @@ public class CalculatorPad extends Fragment {
             } else {
                 numberPad[i].setTextColor(getResources().getColor(R.color.black));
             }
+        }
+
+        for (int i = 0; i < roundedButtonsPad.length; i++) {
+            if (userColor == "orange") {
+                roundedButtonsPad[i].setBackgroundResource(R.drawable.calculator_pad_rounded_orange);
+            } else if (userColor == "red") {
+                roundedButtonsPad[i].setBackgroundResource(R.drawable.calculator_pad_rounded_red);
+            } else if (userColor == "blue") {
+                roundedButtonsPad[i].setBackgroundResource(R.drawable.calculator_pad_rounded_blue);
+            } else if (userColor == "purple") {
+                roundedButtonsPad[i].setBackgroundResource(R.drawable.calculator_pad_rounded_purple);
+            } else if (userColor == "green") {
+                roundedButtonsPad[i].setBackgroundResource(R.drawable.calculator_pad_rounded_green);
+            } else if (userColor == "yellow") {
+                roundedButtonsPad[i].setBackgroundResource(R.drawable.calculator_pad_rounded_yellow);
+            } else {
+                roundedButtonsPad[i].setBackgroundResource(R.drawable.calculator_pad_rounded_purple);
+            }
+        }
+
+        // Change color for special buttons
+
+        if (userColor == "orange") {
+            clean.setBackgroundResource(R.drawable.calculator_pad_rounded_right_orange);
+            powertools.setBackgroundResource(R.drawable.calculator_pad_rounded_left_orange);
+            back.setImageResource(R.drawable.ic_back_orange);
+        } else if (userColor == "red") {
+            clean.setBackgroundResource(R.drawable.calculator_pad_rounded_right_red);
+            powertools.setBackgroundResource(R.drawable.calculator_pad_rounded_left_red);
+            back.setImageResource(R.drawable.ic_back_red);
+        } else if (userColor == "blue") {
+            clean.setBackgroundResource(R.drawable.calculator_pad_rounded_right_blue);
+            powertools.setBackgroundResource(R.drawable.calculator_pad_rounded_left_blue);
+            back.setImageResource(R.drawable.ic_back_blue);
+        } else if (userColor == "purple") {
+            clean.setBackgroundResource(R.drawable.calculator_pad_rounded_right_purple);
+            powertools.setBackgroundResource(R.drawable.calculator_pad_rounded_left_purple);
+            back.setImageResource(R.drawable.ic_back_purple);
+        } else if (userColor == "green") {
+            clean.setBackgroundResource(R.drawable.calculator_pad_rounded_right_green);
+            powertools.setBackgroundResource(R.drawable.calculator_pad_rounded_left_green);
+            back.setImageResource(R.drawable.ic_back_green);
+        } else if (userColor == "yellow") {
+            clean.setBackgroundResource(R.drawable.calculator_pad_rounded_right_yellow);
+            powertools.setBackgroundResource(R.drawable.calculator_pad_rounded_left_yellow);
+            back.setImageResource(R.drawable.ic_back_yellow);
+        } else {
+            clean.setBackgroundResource(R.drawable.calculator_pad_rounded_right_purple);
+            powertools.setBackgroundResource(R.drawable.calculator_pad_rounded_left_purple);
+            back.setImageResource(R.drawable.ic_back_purple);
         }
     }
 
