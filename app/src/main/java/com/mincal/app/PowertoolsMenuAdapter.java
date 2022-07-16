c548be1cd57ebaa3e4eecab0c84331a9b68686c1
Restyled.io <commits@restyled.io>
2022-07-16 23:40:08 +0000

    Restyled by clang-format

--- app/src/main/java/com/mincal/app/PowertoolsMenuAdapter.java
+++ app/src/main/java/com/mincal/app/PowertoolsMenuAdapter.java
@@ -7,48 +7,41 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PowertoolsMenuAdapter extends BaseAdapter {
-    private final Context mContext;
-    private final ArrayList<Integer> powerArray;
+  private final Context mContext;
+  private final ArrayList<Integer> powerArray;
 
-    // Constructor
-    public PowertoolsMenuAdapter(Context c, ArrayList<Integer> array) {
-        mContext = c;
-        powerArray = array;
+  // Constructor
+  public PowertoolsMenuAdapter(Context c, ArrayList<Integer> array) {
+    mContext = c;
+    powerArray = array;
+  }
+
+  public int getCount() { return powerArray.size(); }
+
+  public Object getItem(int position) { return null; }
+
+  public long getItemId(int position) { return 0; }
+
+  // Create a new ImageView for each item referenced by the Adapter
+
+  public View getView(int position, View convertView, ViewGroup parent) {
+    ImageView powertoolIcon;
+    Log.d("Adapter", "Drawable Array: " + powerArray);
+
+    if (convertView == null) {
+      powertoolIcon = new ImageView(mContext);
+      powertoolIcon.setLayoutParams(new GridView.LayoutParams(110, 70));
+      powertoolIcon.setScaleType(ImageView.ScaleType.CENTER);
+      powertoolIcon.setPadding(15, 15, 15, 15);
+    } else {
+      powertoolIcon = (ImageView)convertView;
     }
 
-    public int getCount() {
-        return powerArray.size();
-    }
-
-    public Object getItem(int position) {
-        return null;
-    }
-
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    // Create a new ImageView for each item referenced by the Adapter
-
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView powertoolIcon;
-        Log.d("Adapter", "Drawable Array: " + powerArray);
-
-        if (convertView == null) {
-            powertoolIcon = new ImageView(mContext);
-            powertoolIcon.setLayoutParams(new GridView.LayoutParams(110, 70));
-            powertoolIcon.setScaleType(ImageView.ScaleType.CENTER);
-            powertoolIcon.setPadding(15, 15, 15, 15);
-        } else {
-            powertoolIcon = (ImageView) convertView;
-        }
-
-        powertoolIcon.setImageResource(powerArray.get(position));
-        return powertoolIcon;
-    }
+    powertoolIcon.setImageResource(powerArray.get(position));
+    return powertoolIcon;
+  }
 }
