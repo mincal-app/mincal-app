6128110f788f402bd0db859eb7c9c7c5f0187a68
Restyled.io <commits@restyled.io>
2022-07-17 17:46:56 +0000

    Restyled by clang-format

--- app/src/main/java/com/mincal/app/PowertoolsMenu.java
+++ app/src/main/java/com/mincal/app/PowertoolsMenu.java
@@ -2,14 +2,6 @@ package com.mincal.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityOptionsCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,162 +10,187 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.GridView;
 import android.widget.TextView;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import com.google.common.primitives.Ints;
-
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class PowertoolsMenu extends Fragment {
 
-    // TinyDB
+  // TinyDB
 
-    TinyDB tinydb;
-    ArrayList<String> userPowertools;
+  TinyDB tinydb;
+  ArrayList<String> userPowertools;
+
+  // Views
+
+  TextView categoryButton;
+
+  // Animations
+
+  Animation scaleUp, scaleDown;
+
+  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+  private static final String ARG_PARAM1 = "param1";
+  private static final String ARG_PARAM2 = "param2";
+
+  private String mParam1;
+  private String mParam2;
+
+  public PowertoolsMenu() {
+    // Required empty public constructor
+  }
+
+  public static PowertoolsMenu newInstance(String param1, String param2) {
+    PowertoolsMenu fragment = new PowertoolsMenu();
+    Bundle args = new Bundle();
+    args.putString(ARG_PARAM1, param1);
+    args.putString(ARG_PARAM2, param2);
+    fragment.setArguments(args);
+    return fragment;
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (getArguments() != null) {
+      mParam1 = getArguments().getString(ARG_PARAM1);
+      mParam2 = getArguments().getString(ARG_PARAM2);
+    }
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view,
+                            @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    // Instance of SharedPreferences to store the user's name.
+
+    tinydb = new TinyDB(getContext());
+    userPowertools = tinydb.getListString("user_powertools");
 
     // Views
 
-    TextView categoryButton;
+    categoryButton = getView().findViewById(R.id.category_button);
 
     // Animations
 
-    Animation scaleUp, scaleDown;
+    // Click animations
 
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    scaleUp =
+        AnimationUtils.loadAnimation(getContext(), R.anim.button_click_up);
+    scaleDown =
+        AnimationUtils.loadAnimation(getContext(), R.anim.button_click_down);
 
-    private String mParam1;
-    private String mParam2;
+    // End of Animation Behavior
 
-    public PowertoolsMenu() {
-        // Required empty public constructor
+    scaleDown.setAnimationListener(new Animation.AnimationListener() {
+      @Override
+      public void onAnimationStart(Animation animation) {
+        // Nothing
+      }
+
+      @Override
+      public void onAnimationEnd(Animation animation) {
+        // TODO: Code for changing category icons.
+      }
+
+      @Override
+      public void onAnimationRepeat(Animation animation) {
+        // Nothing
+      }
+    });
+
+    // Getting icons, functions, and ids.
+
+    PowerIcons powerIcons = new PowerIcons(userPowertools);
+    HashMap<String, ArrayList<Integer>> powertoolsMap =
+        powerIcons.getPowertools();
+
+    /* Debug Logs */
+
+    Log.d("PowerIcons",
+          "Original Drawables contains: " + powertoolsMap.values());
+    Log.d("PowerIcons", "TinyDB contains: " + userPowertools);
+    Log.d("PowerIcons", "HashMap contains: " + powertoolsMap);
+
+    // TODO: Look for a way to implement an ArrayList into the adapter as rows.
+
+    ArrayList<Integer> powerDrawables = new ArrayList<>();
+
+    for (String toolKey : powertoolsMap.keySet()) {
+      for (int i = 0; i < powertoolsMap.get(toolKey).size(); i++) {
+        powerDrawables.add(powertoolsMap.get(toolKey).get(i));
+      }
     }
 
-    public static PowertoolsMenu newInstance(String param1, String param2) {
-        PowertoolsMenu fragment = new PowertoolsMenu();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    Log.d("PowerIcons", "Drawables contains: " + powerDrawables);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    // Views
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    GridView powertoolsGrid =
+        (GridView)getView().findViewById(R.id.powertools_grid);
+    TextView hidePowertools = getView().findViewById(R.id.hide_powertools);
 
-        // Instance of SharedPreferences to store the user's name.
+    // Choose icons and PowerTools based on user preferences
 
-        tinydb = new TinyDB(getContext());
-        userPowertools = tinydb.getListString("user_powertools");
+    powertoolsGrid.setAdapter(
+        new PowertoolsMenuAdapter(getContext(), powerDrawables));
 
-        // Views
+    // Hide PowerTools Menu on Close button tap.
 
-        categoryButton = getView().findViewById(R.id.category_button);
+    // Listeners
 
-        // Animations
+    hidePowertools.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        FragmentManager fragmentManager =
+            getActivity().getSupportFragmentManager();
+        FragmentTransaction powerMenuTransaction =
+            fragmentManager.beginTransaction();
+        FragmentTransaction blurTransaction =
+            fragmentManager.beginTransaction();
 
-        // Click animations
+        powerMenuTransaction.setCustomAnimations(
+            R.anim.swipe_out_left, R.anim.dummy_anim_swipe_left,
+            R.anim.dummy_anim_swipe_left, R.anim.swipe_out_left);
+        blurTransaction.setCustomAnimations(
+            R.anim.fade_out, R.anim.dummy_anim_swipe_left,
+            R.anim.dummy_anim_swipe_left, R.anim.fade_out);
 
-        scaleUp = AnimationUtils.loadAnimation(getContext(), R.anim.button_click_up);
-        scaleDown = AnimationUtils.loadAnimation(getContext(), R.anim.button_click_down);
+        Fragment powertoolsMenu =
+            getActivity().getSupportFragmentManager().findFragmentByTag(
+                "blur_powertools_menu");
+        Fragment powertoolsBlur =
+            getActivity().getSupportFragmentManager().findFragmentByTag(
+                "powertools_menu");
+        if (powertoolsMenu != null)
+          powerMenuTransaction.remove(powertoolsMenu).commit();
+        if (powertoolsBlur != null)
+          blurTransaction.remove(powertoolsBlur).commit();
+      }
+    });
 
-        // End of Animation Behavior
+    categoryButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        categoryButton.startAnimation(scaleUp);
+        categoryButton.startAnimation(scaleDown);
+      }
+    });
+  }
 
-        scaleDown.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-                // Nothing
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                // TODO: Code for changing category icons.
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-                // Nothing
-            }
-        });
-
-        // Getting icons, functions, and ids.
-
-        PowerIcons powerIcons = new PowerIcons(userPowertools);
-        HashMap<String, ArrayList<Integer>> powertoolsMap = powerIcons.getPowertools();
-
-        /* Debug Logs */
-
-        Log.d("PowerIcons", "Original Drawables contains: " + powertoolsMap.values());
-        Log.d("PowerIcons", "TinyDB contains: " + userPowertools);
-        Log.d("PowerIcons", "HashMap contains: " + powertoolsMap);
-
-        // TODO: Look for a way to implement an ArrayList into the adapter as rows.
-
-        ArrayList<Integer> powerDrawables = new ArrayList<>();
-
-        for (String toolKey : powertoolsMap.keySet()) {
-            for (int i = 0; i < powertoolsMap.get(toolKey).size(); i++) {
-                powerDrawables.add(powertoolsMap.get(toolKey).get(i));
-            }
-        }
-
-        Log.d("PowerIcons", "Drawables contains: " + powerDrawables);
-
-        // Views
-
-        GridView powertoolsGrid = (GridView) getView().findViewById(R.id.powertools_grid);
-        TextView hidePowertools = getView().findViewById(R.id.hide_powertools);
-
-        // Choose icons and PowerTools based on user preferences
-
-        powertoolsGrid.setAdapter(new PowertoolsMenuAdapter(getContext(), powerDrawables));
-
-        // Hide PowerTools Menu on Close button tap.
-
-        // Listeners
-
-        hidePowertools.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction powerMenuTransaction = fragmentManager.beginTransaction();
-                FragmentTransaction blurTransaction = fragmentManager.beginTransaction();
-
-                powerMenuTransaction.setCustomAnimations(R.anim.swipe_out_left, R.anim.dummy_anim_swipe_left, R.anim.dummy_anim_swipe_left, R.anim.swipe_out_left);
-                blurTransaction.setCustomAnimations(R.anim.fade_out, R.anim.dummy_anim_swipe_left, R.anim.dummy_anim_swipe_left, R.anim.fade_out);
-
-                Fragment powertoolsMenu = getActivity().getSupportFragmentManager().findFragmentByTag("blur_powertools_menu");
-                Fragment powertoolsBlur = getActivity().getSupportFragmentManager().findFragmentByTag("powertools_menu");
-                if(powertoolsMenu!=null) powerMenuTransaction.remove(powertoolsMenu).commit();
-                if(powertoolsBlur!=null) blurTransaction.remove(powertoolsBlur).commit();
-            }
-        });
-
-        categoryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                categoryButton.startAnimation(scaleUp);
-                categoryButton.startAnimation(scaleDown);
-            }
-        });
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_powertools_menu, container, false);
-    }
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    return inflater.inflate(R.layout.fragment_powertools_menu, container,
+                            false);
+  }
 }
